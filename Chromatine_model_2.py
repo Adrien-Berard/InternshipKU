9e012ce09a61b165a49779369b8389c055d106ff
AdrienBerard <berardadrien7@gmail.com>
2024-02-05 12:00:48 +0100

    Update Chromatine_model_2.py

--- Chromatine_model_2.py
+++ Chromatine_model_2.py
@@ -10,7 +10,10 @@ class Chromatine:
 
     def regenerate_histones(self, positions):
         # Replace histones at specified positions with new histones ('A')
-        self.histones[positions] = 'A'
+        positions = np.atleast_1d(positions)
+        for pos in positions:
+            if 0 <= int(pos) < self.histones.shape[0]:
+                self.histones[int(pos)] = 'A'
 
     def add_polymerases(self, count, existing_polymerase_positions, adding_position):
         # Add a specified number of new polymerases at non-overlapping positions
@@ -26,7 +29,7 @@ class Chromatine:
         local_density = np.sum(np.isin(self.histones[start_index:end_index], ['M', 'A']))
 
         slope = 1e-5
-        intercept = 1e-2
+        intercept = 1e-1
         probability = slope * local_density + intercept
 
         return probability
@@ -71,20 +74,20 @@ class Polymerase:
     def delete(self):
         polymerases.remove(self)
 
-    def move(self, chromatine):
+    def move(self):
         states = [0, 1]
-        next_positions = self.position + states
-        probabilities = [1/2, 30]
+        next_positions = np.array(self.position) + states
+        probabilities = [1/2, 1/2]  # Fixed probabilities for each state
         total_prob = sum(probabilities)
         normalized_probabilities = [prob / total_prob for prob in probabilities]
         self.position = np.random.choice(next_positions, p=normalized_probabilities)
 
-        if self.position == chromatine.histones.shape[0]:
+        if self.position == self.chromatine.histones.shape[0]:
             self.delete()
 
-    def change_histones(self, chromatine):
-        if 0 <= self.position < chromatine.histones.shape[0] and chromatine.histones[self.position] == 'M':
-            chromatine.histones[self.position] = 'A'
+    def change_histones(self):
+        if 0 <= self.position < self.chromatine.histones.shape[0] and self.chromatine.histones[self.position] == 'M':
+            self.chromatine.histones[self.position] = 'A'
 
 def visualize_chromatine(histones, polymerase_positions=None):
     plt.bar(range(histones.shape[0]), [1] * histones.shape[0],
@@ -93,24 +96,27 @@ def visualize_chromatine(histones, polymerase_positions=None):
     plt.xlabel("Histone Position")
     plt.ylabel("Histone")
 
-    if polymerase_positions:
+    if polymerase_positions is not None and np.any(polymerase_positions):
         for position in polymerase_positions:
             plt.arrow(position, -0.2, 0, -0.1, color='red', head_width=0.5, head_length=0.1)
 
 def update(frame):
-    deleted_positions = np.array('')
-    polymerase_positions = np.array('')
+    global polymerase_count_over_time, active_histone_count_over_time, acetylated_histone_count_over_time, methylated_histone_count_over_time
+    global polymerases
+
+    deleted_positions = np.array(0)
+    polymerase_positions = np.array(0)
 
     for polymerase in polymerases:
-        polymerase.move(chromatine)
-        polymerase.change_histones(chromatine)
+        polymerase.move()
+        polymerase.change_histones()
         deleted_positions = np.append(deleted_positions, polymerase.position)
         polymerase_positions = np.append(polymerase_positions, polymerase.position)
 
     if random.random() < chromatine.adding_poly_proba(adding_position):
         chromatine.add_polymerases(1, existing_polymerase_positions, adding_position)
         new_polymerase_positions = existing_polymerase_positions[-1:]
-        new_polymerases = np.array([Polymerase(chromatine, position=pos, temperature=1.0) for pos in new_polymerase_positions])
+        new_polymerases = np.array([Polymerase(chromatine, position=int(pos), temperature=1.0) for pos in new_polymerase_positions])
         polymerases = np.concatenate([polymerases, new_polymerases])
 
     chromatine.regenerate_histones(deleted_positions)
@@ -125,11 +131,6 @@ def update(frame):
     active_histone_count_over_time = np.append(active_histone_count_over_time, active_histone_count)
     acetylated_histone_count_over_time = np.append(acetylated_histone_count_over_time, acetylated_histone_count)
     methylated_histone_count_over_time = np.append(methylated_histone_count_over_time, methylated_histone_count)
-
-    polymerase_count_over_time = np.pad(polymerase_count_over_time, (0, frame + 2 - polymerase_count_over_time.shape[0]), 'constant')
-    acetylated_histone_count_over_time = np.pad(acetylated_histone_count_over_time, (0, frame + 1 - acetylated_histone_count_over_time.shape[0]), 'constant')
-    methylated_histone_count_over_time = np.pad(methylated_histone_count_over_time, (0, frame + 1 - methylated_histone_count_over_time.shape[0]), 'constant')
-    active_histone_count_over_time = np.pad(active_histone_count_over_time, (0, frame + 1 - active_histone_count_over_time.shape[0]), 'constant')
 
     axs[0, 0].clear()
     axs[0, 1].clear()
@@ -150,19 +151,21 @@ def update(frame):
 
     visualize_chromatine(chromatine.histones, polymerase_positions=polymerase_positions)
 
+# Parameters for simulation
 chromatine_size = 50
-polymerase_count = 0
+polymerase_count = 1
 simulation_steps = 100
 adding_position = 25
 
+# Initialize chromatine and polymerases with a specified temperature
 chromatine = Chromatine(chromatine_size)
 polymerases = np.array([Polymerase(chromatine, temperature=1.0) for _ in range(polymerase_count)])
 existing_polymerase_positions = np.array([polymerase.position for polymerase in polymerases])
 
-polymerase_count_over_time = np.array('')
-active_histone_count_over_time = np.array('')
-acetylated_histone_count_over_time = np.array('')
-methylated_histone_count_over_time = np.array('')
+polymerase_count_over_time = np.array([polymerase_count])
+active_histone_count_over_time = np.array([chromatine_size])
+acetylated_histone_count_over_time = np.array([0])
+methylated_histone_count_over_time = np.array([chromatine_size])
 
 fig, axs = plt.subplots(2, 2, figsize=(12, 8))
 ani = FuncAnimation(fig, update, frames=simulation_steps, repeat=False)
