1dcdc3b6006c4e2352654fb52721f9d4b313095f
Adrien Berard <berardadrien7@gmail.com>
2024-02-12 10:53:34 +0100

    Update plotFigure3PaperKim2007.py

--- ModellingChromatine/plotFigure3PaperKim2007.py
+++ ModellingChromatine/plotFigure3PaperKim2007.py
@@ -6,7 +6,7 @@ import seaborn as sns
 csv_filename = 'fig3_C_coop_counting_lists_dataframe_polymerasecount_0_alpha_0.9871794871794872_F_77_addingpolyprobaintercept_0_addingpolyprobaslope_0.csv'
 df = pd.read_csv(csv_filename)
 
-sns.kdeplot(df['Methylated Histone Count'] - df['Acetylated Histone Count'])
+sns.kdeplot(df['Methylated Histone Count'] - df['Acetylated Histone Count'],multiple = 'stack')
 
 plt.xlabel('M-A')
 plt.ylabel('P(M-A)')
